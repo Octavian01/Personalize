3aa780b6b59c0ca26895001af51e41f3e40139a4
Octavian <octavian4ik.mitu@gmail.com>
2020-06-21 13:26:23 +0300

    C

--- app.js
+++ app.js
@@ -65,7 +65,6 @@ app.use((req, res, next) => {
             basket: { items: [] },
             likesItems: []
         }
-        console.log(req.session);
         return next();
     } else return next();
 
